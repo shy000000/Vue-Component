498c33f3f6424e9e47b6d677680474bd27d15952
lihuibin <751238701@qq.com>
2017-01-19 17:03:31 +0800

    anthor:lihuibin review:lihuibin

---
+++ Module/sumiao/scripts/index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var mycanvas = document.getElementById('mycanvas'),
+	    ctx = mycanvas.getContext('2d'),
+	    img = document.getElementById('img');
+	ctx.drawImage(img,0,0,400,400);
+	var imgData = ctx.getImageData(0,0,500,500);
+	console.log(imgData);
+}
